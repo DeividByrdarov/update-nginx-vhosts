deb0f3b0643ffc07464fddc9b76282f61faeae15
Deivid Byrdarov <deivid.byrdarov@gmail.com>
2019-06-14 16:30:55 +0300

    Creating vHost files

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const fs = require("fs-extra")
 const path = require("path")
 const inquirer = require("inquirer")
-const colors = require("colors")
+
+require("colors")
 
 const cwd = process.cwd()
 ;(async () => {
@@ -19,47 +20,79 @@ const cwd = process.cwd()
   fs.readFile(templatePath, "utf8", async (err, template) => {
     if (err) {
       if (err.code === "ENOENT") {
-        console.log("File not found!".red)
+        console.log("\nFile not found!\n".red)
         return
       }
       throw err
     }
-    const directory = "/etc/nginx/sites-available"
-    if (!fs.existsSync(directory)) {
-      console.log("Nginx has not been installed.".red)
+
+    const availableDirectory = "/etc/nginx/sites-available"
+    const enabledDirectory = "/etc/nginx/sites-enabled"
+
+    if (!fs.existsSync(enabledDirectory)) {
+      console.log("\nNginx has not been installed.\n".red)
       return
     }
 
-    const hostFiles = fs.readdirSync(directory)
-
-    for (const file of hostFiles) {
-      if (file === "default") {
-        console.log("Skipped deleting default vHost".green)
-        continue
-      }
-      fs.unlink(path.join(directory, file), err => {
-        if (err) throw err
-      })
-    }
+    deleteHosts(availableDirectory)
+    deleteHosts(enabledDirectory)
 
     for (let name of files) {
       if (fs.statSync(path.join(cwd, name)).isDirectory()) {
-        const { indexFile } = await inquirer.prompt([
-          {
-            type: "input",
-            name: "indexFile",
-            default: "index.html",
-            message: `Enter an index file for ${name}: `,
-          },
-        ])
-        let newTemplate = "" + template
-        const logFile = name.split(".")[0]
-        newTemplate = newTemplate.split("$!{name}").join(name)
-        newTemplate = newTemplate.split("$!{logFile}").join(logFile)
-        newTemplate = newTemplate.split("$!{indexFile}").join(indexFile)
-        console.log(newTemplate)
-        console.log("-----------------------------------")
+        const newTemplate = await replacePlaceholders(template, name)
+
+        createVhost(newTemplate, name)
       }
     }
   })
 })()
+
+const createVhost = (template, name) => {
+  const vhostFile = "/" + path.join("etc", "nginx", "sites-available", name)
+  fs.writeFile(vhostFile, template, err => {
+    if (err) throw err
+    console.log(`Created vHost ${name}`.green)
+    fs.symlink(
+      vhostFile,
+      "/" + path.join("etc", "nginx", "sites-enabled", name),
+      err => {
+        if (err) throw error
+        console.log(`Created symlink for vHost ${name}`.green)
+      }
+    )
+  })
+}
+
+const replacePlaceholders = async (template, name) => {
+  const { indexFile } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "indexFile",
+      default: "index.html",
+      message: `Enter an index file for ${name}: `,
+    },
+  ])
+
+  let newTemplate = "" + template
+  const logFile = name.split(".")[0]
+
+  newTemplate = newTemplate.split("$!{name}").join(name)
+  newTemplate = newTemplate.split("$!{logFile}").join(logFile)
+  newTemplate = newTemplate.split("$!{indexFile}").join(indexFile)
+
+  return newTemplate
+}
+
+const deleteHosts = directory => {
+  const hostFiles = fs.readdirSync(directory)
+
+  for (const file of hostFiles) {
+    if (file === "default") {
+      console.log("Skipped deleting default vHost".green)
+      continue
+    }
+    fs.unlink(path.join(directory, file), err => {
+      if (err) throw err
+    })
+  }
+}
