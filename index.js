51d8b9b85bb4d1b702a4d9a0c32c4a5afe06d9df
Deivid Byrdarov <deivid.byrdarov@gmail.com>
2019-06-17 11:03:00 +0300

    Add a second option to add just a single project as vHost, ask if they want to delete previous vHosts

--- index.js
+++ index.js
@@ -7,13 +7,89 @@ const exec = require("child_process").exec
 require("colors")
 
 const cwd = process.cwd()
-;(async () => {
-  const { templatePath } = await inquirer.prompt([
+
+const SELECTIONS = {
+  ALL_FOLDERS: "Get all folders from current directory as vHosts",
+  CHOOSE_FOLDER: "Choose folder to add as vHost",
+}
+
+const main = async () => {
+  const { selection } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "selection",
+      message: "What would you like to do?",
+      choices: [...Object.keys(SELECTIONS).map(name => SELECTIONS[name])],
+    },
+  ])
+
+  switch (selection) {
+    case SELECTIONS.ALL_FOLDERS:
+      updateAllVhosts()
+      break
+    case SELECTIONS.CHOOSE_FOLDER:
+      selectFolderForVhost()
+      break
+    default:
+      console.log("Something went wrong".red)
+      break
+  }
+}
+
+const selectFolderForVhost = async () => {
+  const files = await fs.readdir(cwd)
+  const choices = []
+
+  for (const file of files) {
+    if (fs.statSync(path.join(cwd, file)).isDirectory()) {
+      choices.push(file)
+    }
+  }
+
+  const { name } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "name",
+      message: "Select project that you want to add to vHosts",
+      choices,
+    },
+  ])
+
+  const { templatePath } = await askForTemplate()
+
+  fs.readFile(templatePath, "utf8", async (err, template) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        console.log("\nFile not found!\n".red)
+        return
+      }
+      throw err
+    }
+
+    const newTemplate = await replacePlaceholders(template, name)
+
+    await createVhost(newTemplate, name)
+  })
+}
+
+const askForTemplate = async () =>
+  await inquirer.prompt([
     {
       type: "input",
       name: "templatePath",
       default: "/etc/nginx/template",
       message: "Enter a template file path: ",
+    },
+  ])
+
+const updateAllVhosts = async () => {
+  const { templatePath } = await askForTemplate()
+  const { deleteVhosts } = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "deleteVhosts",
+      message: "Should we delete all vHosts except the default one?",
+      default: true,
     },
   ])
 
@@ -27,16 +103,18 @@ const cwd = process.cwd()
       throw err
     }
 
-    const availableDirectory = "/etc/nginx/sites-available"
-    const enabledDirectory = "/etc/nginx/sites-enabled"
-
-    if (!fs.existsSync(enabledDirectory)) {
-      console.log("\nNginx has not been installed.\n".red)
+    if (!nginxInstalled()) {
       return
     }
 
-    deleteHosts(availableDirectory)
-    deleteHosts(enabledDirectory)
+    if (deleteHosts) {
+      const availableDirectory =
+        "/" + path.join("etc", "nginx", "sites-available")
+      const enabledDirectory = "/" + path.join("etc", "nginx", "sites-enabled")
+
+      deleteHosts(availableDirectory)
+      deleteHosts(enabledDirectory)
+    }
 
     for (let name of files) {
       if (fs.statSync(path.join(cwd, name)).isDirectory()) {
@@ -53,9 +131,24 @@ const cwd = process.cwd()
       console.log("Nginx restarted... Go check your new HOSTS 😉".green)
     })
   })
-})()
+}
+
+const nginxInstalled = () => {
+  const enabledDirectory = "/" + path.join("etc", "nginx", "sites-available")
+
+  if (!fs.existsSync(enabledDirectory)) {
+    console.log("\nNginx has not been installed.\n".red)
+    return 0
+  }
+
+  return 1
+}
 
 const createVhost = async (template, name) => {
+  if (!nginxInstalled()) {
+    return
+  }
+
   const vhostFile = "/" + path.join("etc", "nginx", "sites-available", name)
   fs.writeFileSync(vhostFile, template)
   console.log(`Created vHost ${name}`.green)
@@ -99,3 +192,5 @@ const deleteHosts = directory => {
     })
   }
 }
+
+main()
